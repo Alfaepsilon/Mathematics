cee0f76983201d8b212410bc759d67757d08ecb3
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2024-10-28 20:11:52 +0100

    Update limz.c

--- Calculus/limz.c
+++ Calculus/limz.c
@@ -3,8 +3,8 @@
 
 #define lim(x) (x * sin(1/x))
 
-//Calculates limit by, if limit is 0, inserting (j)^n into the limit-function where j < 1 and n is the current iteration and, if limit is k != 0, 
-// inserting k ^ (1 + (j)^n) where j < 1 and n is the current iteration. Should for left and right limits be able to change sign of j. Prob will get weird output for some functions where limits are infinity.
+//Calculates limit by, if limit is 0, inserting (j)^n into the limit-function where 0 < j < 1 and n is the current iteration and, if limit is k != 0, 
+// inserting k ^ (1 + (j)^n) where 0 < j < 1 and n is the current iteration. Should for left and right limits be able to change sign of j. Prob will get weird output for some functions where limits are infinity.
 int main() {
 	int limit, iterations, i = 0;
 	float j = 0.1;
