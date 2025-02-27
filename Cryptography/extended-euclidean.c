a2844c7e315e4f10a397c42d224b6874a58771f0
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-02-27 20:55:11 +0100

    Create extended-euclidean.c
    Extended euclidean algorithm

---
+++ Cryptography/extended-euclidean.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	int a;
+	int b;
+	int u = 1;
+	int x = 0;
+	int s;
+	int t;
+	int q;
+	printf("Type a number: \n");
+	scanf_s("%d", &a);
+	int g = a;
+	printf("Type another number: \n");
+	scanf_s("%d", &b);
+	if (b == 0) {
+		printf("%d, %d, %d", g, u, 0);
+		return 0;
+	}
+	int y = b;
+	while (y > 0) {
+		t = g % y;
+		q = g / y;
+		s = u - (q * x);
+		u = x;
+		g = y;
+		x = s;
+		y = t;
+	}
+	int v = (g - a * u) / b;
+	while (u < 0) {
+		u = u + b/g;
+		v = v - a/g;
+	}
+	printf("%d, %d, %d",  g, u, v);
+	return 0;
+}
