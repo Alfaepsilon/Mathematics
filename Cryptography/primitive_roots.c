3d87bdb008e6a754d71a758e880072af3ffe8283
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-03-28 18:58:39 +0100

    Update primitive_roots.c

--- Cryptography/primitive_roots.c
+++ Cryptography/primitive_roots.c
@@ -9,19 +9,15 @@ int primitive_roots(int p) {
 	for (int i = 2; i < p; i++) {
 		for (int j = 0; j < p - 1; j++) {
 			prim_roots[j] = fastpoweralgo_lowmem(p, i, j);
-		}
-		for (int h = 1; h < p; h++) {
-			for (int k = 0; k < p - 1; k++) {
-				if (prim_roots[k] == h) {
-					count = count + 1;
-				}
-				if (count > 1) {
-					count = 0;
-					goto skip;
-				}
+			if (prim_roots[j] == i) {
+				count = count + 1;
 			}
-			count = 0;
+			if (count > 1) {
+				count = 0;
+				goto skip;
+			}
 		}
+		count = 0;
 		printf("%d is a primitive root \n", i);
 		skip:
 			continue;
