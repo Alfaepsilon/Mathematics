cf990f15464b70ecd59177679f2053942ea55720
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-05-05 14:17:40 +0200

    Update shanks_baby_giant.c

--- Cryptography/shanks_baby_giant.c
+++ Cryptography/shanks_baby_giant.c
@@ -3,18 +3,18 @@
 #include <math.h>
 #include "crypto.h"
 
-int shanks(int val, int p, int g) {
-	int inverse = exeucalgo(g, p, 1);
-	int N = DLP(1, p, g);
-	int n = 1 + floor(sqrt(N));
-	int l1[n];
-	for(int i = 0; i < n + 1; i++){
+unsigned long long main(unsigned long long val, unsigned long long p, unsigned long long g) {
+	unsigned long long inverse = exeucalgo(g, p, 1);
+	unsigned long long N = DLP(1, p, g);
+	unsigned long long n = 1 + floor(sqrt(N));
+	unsigned long long l1[n];
+	for(unsigned long long i = 0; i < n; i++){
 		l1[i] = fastpoweralgo_lowmem(p, g, i);
 	}
-	for(int i = 0; i < n + 1; i++){
-		for(int j = 0; j < n + 1; j++){
+	for(unsigned long long i = 0; i < n; i++){
+		for(unsigned long long j = 0; j < n; j++){
 			if((val * fastpoweralgo_lowmem(p, inverse, i * n) % p) == l1[j]){
-				return j + i*n;
+				printf("%llu", j + i*n);
 			}
 		}
 	}
